3266d616874c99a38c3dde5f9011571e07adac01
carlosholden <113494161+carlosholden@users.noreply.github.com>
2022-10-13 08:57:35 -0500

    Add files via upload

---
+++ Carlos/variables.php
@@ -0,0 +1,15 @@
+<?php
+//Ejemplos de nombres de variables:
+$edad = 16;
+$edad1 = 17;
+$edad_1 = 18;
+
+// Ejemplos de nombres de variables (estilos no recomendados):
+
+$años = 19;
+$_edad = 20;
+$Edad = 21;
+$__edad = 22;
+$EDAD = 23;
+$_EDAD = 24;
+?>
